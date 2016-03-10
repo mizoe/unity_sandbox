7c841dcff04c6e48f5cab3a190ca7aa8802f7c03
ShiroJun <0909robinson@gimail.com>
2016-03-10 19:51:52 +0900

    s v4

---
+++ unity_sandbox/Assets/Szuki/DestroyBall.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBall : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision c)
+    {
+        Destroy(c.gameObject);
+    }
+}
