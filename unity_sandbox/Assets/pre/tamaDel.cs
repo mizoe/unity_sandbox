0a7e5f0cf878ba476d98951b399f85701d6750f6
ds2223 <ds22232223@gmail.com>
2016-03-10 19:31:13 +0900

    tama-

---
+++ unity_sandbox/Assets/pre/tamaDel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tamaDel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("Destroy01", 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void Destroy01(){
+		Destroy (gameObject);
+	}
+}
