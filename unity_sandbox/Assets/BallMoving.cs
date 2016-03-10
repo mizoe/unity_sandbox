beb43e6acd0e9476b7c00652f5d7995530389707
masanoritakano <masanori77t@gmail.com>
2016-03-10 19:20:11 +0900

    takano Ball追加

---
+++ unity_sandbox/Assets/BallMoving.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMoving : MonoBehaviour {
+    public float speed = 1;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 10),transform.position.y, transform.position.z);
+    }
+}
